fb86cb5b9694186367d03b0871e50ff5ec416d48
贾一饼 <lby.i@qq.com>
2024-08-05 07:03:00 +0800

    doc: TransformPoint

--- math/coordinate.go
+++ math/coordinate.go
@@ -3,7 +3,14 @@ package math
 import "math"
 
 // 两个原点不重合的坐标系O1,O2,均为x轴向右，y轴向下。O2在O1内部,且经过顺时针旋转c度。其中的点分别用(x1,y1),(x2,y2)表示，已知某个点在两个坐标系中的坐标(x1,y1),(x2,y2),以及另一点在O2内的坐标(x2,y2)，求该点在O1内的坐标(x1,y1)
-
+// ------------------------------
+// |         、
+// |       /     、
+// |      /          、
+// |     /               、
+// |    /
+// |
+// |
 // Point 结构体用于表示一个点
 type Point struct {
 	X float64
@@ -11,7 +18,7 @@ type Point struct {
 }
 
 // TransformPoint transforms a point from coordinate system a2 to a1
-func TransformPoint(p, p1, p2 Point, angleDeg float64) Point {
+func TransformPoint(p1InO2, p2InO1, p2InO2 Point, angleDeg float64) Point {
 
 	// Convert angle from degrees to radians
 	angleRad := angleDeg * math.Pi / 180.0
@@ -20,12 +27,12 @@ func TransformPoint(p, p1, p2 Point, angleDeg float64) Point {
 	sinC := math.Sin(angleRad)
 
 	// Calculate dx and dy
-	dx := p1.X - (p2.X*cosC - p2.Y*sinC)
-	dy := p1.Y - (p2.X*sinC + p2.Y*cosC)
+	dx := p2InO1.X - (p2InO2.X*cosC - p2InO2.Y*sinC)
+	dy := p2InO1.Y - (p2InO2.X*sinC + p2InO2.Y*cosC)
 
 	// Apply rotation and translation
-	x1 := p.X*cosC - p.Y*sinC + dx
-	y1 := p.X*sinC + p.Y*cosC + dy
+	x1 := p1InO2.X*cosC - p1InO2.Y*sinC + dx
+	y1 := p1InO2.X*sinC + p1InO2.Y*cosC + dy
 
 	return Point{x1, y1}
 }
