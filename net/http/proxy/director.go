8a9eada5dd34b71cdebfdc1403bf9a246ebf73bf
贾一饼 <lby.i@qq.com>
2024-09-27 15:10:59 +0800

    feat: proxy

---
+++ net/http/proxy/director.go
@@ -0,0 +1,38 @@
+package proxy
+
+import (
+	"github.com/rs/cors"
+	"net/http"
+	"net/http/httputil"
+	"net/url"
+)
+
+func Director(addr string) error {
+	proxy := &httputil.ReverseProxy{
+		Director: func(r *http.Request) {
+			targets := r.Header["Target-Url"]
+			if len(targets) == 0 {
+				return
+			}
+			target := targets[0]
+			targetUrl, _ := url.Parse(target)
+			r.Host = targetUrl.Host
+			r.URL.Host = targetUrl.Host
+			r.URL.Scheme = targetUrl.Scheme
+
+			r.Header["Refer"] = r.Header["Target-Refer"]
+			r.Header["Origin"] = r.Header["Target-Origin"]
+		},
+		Transport: &http.Transport{
+			Proxy:             http.ProxyFromEnvironment, // 代理使用
+			ForceAttemptHTTP2: true,
+		},
+		ModifyResponse: func(resp *http.Response) error {
+			delete(resp.Header, "Access-Control-Allow-Origin")
+			return nil
+		},
+	}
+	server := cors.AllowAll()
+
+	return http.ListenAndServe(addr, server.Handler(proxy))
+}
---
+++ net/http/proxy/rewrite.go
@@ -0,0 +1,39 @@
+package proxy
+
+import (
+	"github.com/rs/cors"
+	"net/http"
+	"net/http/httputil"
+	"net/url"
+)
+
+func Rewrite(addr string) error {
+	proxy := &httputil.ReverseProxy{
+		Rewrite: func(r *httputil.ProxyRequest) {
+			targets := r.In.Header["Target-Url"]
+			if len(targets) == 0 {
+				return
+			}
+			target := targets[0]
+			targetUrl, _ := url.Parse(target)
+			r.Out.URL = r.In.URL
+			r.Out.Host = targetUrl.Host
+			r.Out.URL.Host = targetUrl.Host
+			r.Out.URL.Scheme = targetUrl.Scheme
+
+			r.Out.Header["Refer"] = r.In.Header["Target-Refer"]
+			r.Out.Header["Origin"] = r.In.Header["Target-Origin"]
+		},
+		Transport: &http.Transport{
+			Proxy:             http.ProxyFromEnvironment, // 代理使用
+			ForceAttemptHTTP2: true,
+		},
+		ModifyResponse: func(resp *http.Response) error {
+			delete(resp.Header, "Access-Control-Allow-Origin")
+			return nil
+		},
+	}
+	server := cors.AllowAll()
+
+	return http.ListenAndServe(addr, server.Handler(proxy))
+}
