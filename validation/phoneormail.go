5df214a75fcbad627be77b8677ce33e14ca5c47c
贾一饼 <lby.i@qq.com>
2024-12-16 23:24:39 +0800

    fix: validation:isPhone

--- validation/phoneormail.go
+++ validation/phoneormail.go
@@ -13,16 +13,25 @@ const (
 	Mail
 	Unknown
 )
+const (
+	emailPattern = `^[a-zA-Z0-9.!#$%&'*+/=?^_` + "`" + `{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$`
+	phonePattern = `^1[0-9]{10}$`
+)
 
 func PhoneOrMail(input string) int {
-	emailMatch, _ := regexp.MatchString(`^([a-zA-Z0-9]+[_.]?)*[a-zA-Z0-9]+@([a-zA-Z0-9]+[_.]?)*[a-zA-Z0-9]+.[a-zA-Z]{2,3}$`, input)
-	if emailMatch {
-		return Mail
+	phoneMatch, _ := regexp.MatchString(phonePattern, input)
+	if phoneMatch {
+		return Phone
 	} else {
-		phoneMatch, _ := regexp.MatchString(`^1[0-9]{10}$`, input)
-		if phoneMatch {
-			return Phone
+		emailMatch, _ := regexp.MatchString(emailPattern, input)
+		if emailMatch {
+			return Mail
 		}
 	}
 	return Unknown
+}
+
+func IsPhone(input string) bool {
+	phoneMatch, _ := regexp.MatchString(phonePattern, input)
+	return phoneMatch
 }
