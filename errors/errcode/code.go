e8cda8e4f0bee9fb446286efb59ea5ca60055962
贾一饼 <lby.i@qq.com>
2024-12-20 22:02:29 +0800

    optimize: errcode

--- errors/errcode/code.go
+++ errors/errcode/code.go
@@ -8,10 +8,23 @@ package errcode
 
 const (
 	// SysErr ErrCode = -1
-	Success         ErrCode = 0
-	Canceled        ErrCode = 1
-	Unknown         ErrCode = 2
-	InvalidArgument ErrCode = 3
+	Success            ErrCode = 0
+	Canceled           ErrCode = 1
+	Unknown            ErrCode = 2
+	InvalidArgument    ErrCode = 3
+	DeadlineExceeded   ErrCode = 4
+	NotFound           ErrCode = 5
+	AlreadyExists      ErrCode = 6
+	PermissionDenied   ErrCode = 7
+	ResourceExhausted  ErrCode = 8
+	FailedPrecondition ErrCode = 9
+	Aborted            ErrCode = 10
+	OutOfRange         ErrCode = 11
+	Unimplemented      ErrCode = 12
+	Internal           ErrCode = 13
+	Unavailable        ErrCode = 14
+	DataLoss           ErrCode = 15
+	Unauthenticated    ErrCode = 16
 )
 
 var codeMap = make(map[ErrCode]string)
@@ -24,5 +37,18 @@ func RegisterErrCode(code ErrCode, msg string) {
 func init() {
 	RegisterErrCode(Canceled, "Canceled")
 	RegisterErrCode(Unknown, "Unknown")
-	RegisterErrCode(InvalidArgument, "Invalid Argument")
+	RegisterErrCode(InvalidArgument, "InvalidArgument")
+	RegisterErrCode(DeadlineExceeded, "DeadlineExceeded")
+	RegisterErrCode(NotFound, "NotFound")
+	RegisterErrCode(AlreadyExists, "AlreadyExists")
+	RegisterErrCode(PermissionDenied, "PermissionDenied")
+	RegisterErrCode(ResourceExhausted, "ResourceExhausted")
+	RegisterErrCode(FailedPrecondition, "FailedPrecondition")
+	RegisterErrCode(Aborted, "Aborted")
+	RegisterErrCode(OutOfRange, "OutOfRange")
+	RegisterErrCode(Unimplemented, "Unimplemented")
+	RegisterErrCode(Internal, "Internal")
+	RegisterErrCode(Unavailable, "Unavailable")
+	RegisterErrCode(DataLoss, "DataLoss")
+	RegisterErrCode(Unauthenticated, "Unauthenticated")
 }
