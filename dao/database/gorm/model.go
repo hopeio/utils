2247c4b50cfbcff952b2e335e00fde40a2755fd1
贾一饼 <lby.i@qq.com>
2024-12-29 23:51:19 +0800

    fix: gorm model

--- dao/database/gorm/model.go
+++ dao/database/gorm/model.go
@@ -9,5 +9,12 @@ type Model struct {
 	ID        uint           `json:"id" gorm:"primarykey"`
 	CreatedAt time.Time      `json:"createdAt"`
 	UpdatedAt time.Time      `json:"updatedAt"`
+	DeletedAt gorm.DeletedAt `json:"-" gorm:"index"`
+}
+
+type ModelDelTime struct {
+	ID        uint           `json:"id" gorm:"primarykey"`
+	CreatedAt time.Time      `json:"createdAt"`
+	UpdatedAt time.Time      `json:"updatedAt"`
 	DeletedAt gorm.DeletedAt `json:"deletedAt" gorm:"index"`
 }
