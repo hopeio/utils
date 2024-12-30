5e273b5c1627585dcf2f15efe6dcf558efd229bb
贾一饼 <lby.i@qq.com>
2024-12-30 12:07:50 +0800

    chore: remove apollo client

--- dao/database/gorm/model.go
+++ dao/database/gorm/model.go
@@ -9,12 +9,5 @@ type Model struct {
 	ID        uint           `json:"id" gorm:"primarykey"`
 	CreatedAt time.Time      `json:"createdAt"`
 	UpdatedAt time.Time      `json:"updatedAt"`
-	DeletedAt gorm.DeletedAt `json:"-" gorm:"index"`
-}
-
-type ModelDelTime struct {
-	ID        uint           `json:"id" gorm:"primarykey"`
-	CreatedAt time.Time      `json:"createdAt"`
-	UpdatedAt time.Time      `json:"updatedAt"`
-	DeletedAt gorm.DeletedAt `json:"deletedAt" gorm:"index"`
+	DeletedAt gorm.DeletedAt `json:"deletedAt,omitempty" gorm:"index"`
 }
--- types/model/model.go
+++ types/model/model.go
@@ -8,14 +8,7 @@ type Model struct {
 	ID        uint      `json:"id" gorm:"primarykey"`
 	CreatedAt time.Time `json:"createdAt"`
 	UpdatedAt time.Time `json:"updatedAt"`
-	DeletedAt time.Time `json:"-" gorm:"index"`
-}
-
-type ModelDelTime struct {
-	ID        uint      `json:"id" gorm:"primarykey"`
-	CreatedAt time.Time `json:"createdAt"`
-	UpdatedAt time.Time `json:"updatedAt"`
-	DeletedAt time.Time `json:"deletedAt" gorm:"index"`
+	DeletedAt time.Time `json:"deletedAt,omitempty" gorm:"index"`
 }
 
 type ModelExt struct {
