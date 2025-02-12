9dfcdf115eca1d6ddc04f69d08f40502f742ca70
贾一饼 <lby.i@qq.com>
2025-02-12 18:33:27 +0800

    fix: validator

--- validate/validator/helper.go
+++ validate/validator/helper.go
@@ -6,6 +6,8 @@
 
 package validator
 
+import "strings"
+
 // Validator is a general interface that allows a message to be validated.
 type IValidator interface {
 	Validate() error
@@ -16,4 +18,25 @@ func CallValidatorIfExists(candidate interface{}) error {
 		return validator.Validate()
 	}
 	return nil
+}
+
+type fieldError struct {
+	fieldStack []string
+	nestedErr  error
+}
+
+func (f *fieldError) Error() string {
+	return "invalid field " + strings.Join(f.fieldStack, ".") + ": " + f.nestedErr.Error()
+}
+
+// FieldError wraps a given Validator error providing a message call stack.
+func FieldError(fieldName string, err error) error {
+	if fErr, ok := err.(*fieldError); ok {
+		fErr.fieldStack = append([]string{fieldName}, fErr.fieldStack...)
+		return err
+	}
+	return &fieldError{
+		fieldStack: []string{fieldName},
+		nestedErr:  err,
+	}
 }
