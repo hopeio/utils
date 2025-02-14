0d102ac69875f0d4c7c058249f2d6bf4fdca3e95
贾一饼 <lby.i@qq.com>
2025-02-14 14:14:19 +0800

    feat: errcode to httpstatus

--- errors/errcode/errcode.go
+++ errors/errcode/errcode.go
@@ -9,6 +9,7 @@ package errcode
 import (
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
+	"net/http"
 	"strconv"
 )
 
@@ -49,4 +50,45 @@ func (x ErrCode) Wrap(err error) *ErrRep {
 
 func (x ErrCode) Error() string {
 	return x.String()
+}
+
+func (code ErrCode) HttpStatus() int {
+	switch code {
+	case Success:
+		return http.StatusOK
+	case Canceled:
+		return http.StatusRequestTimeout
+	case Unknown:
+		return http.StatusInternalServerError
+	case InvalidArgument:
+		return http.StatusBadRequest
+	case DeadlineExceeded:
+		return http.StatusGatewayTimeout
+	case NotFound:
+		return http.StatusNotFound
+	case AlreadyExists:
+		return http.StatusConflict
+	case PermissionDenied:
+		return http.StatusForbidden
+	case Unauthenticated:
+		return http.StatusUnauthorized
+	case ResourceExhausted:
+		return http.StatusTooManyRequests
+	case FailedPrecondition:
+		// Note, this deliberately doesn't translate to the similarly named '412 Precondition Failed' HTTP response status.
+		return http.StatusBadRequest
+	case Aborted:
+		return http.StatusConflict
+	case OutOfRange:
+		return http.StatusBadRequest
+	case Unimplemented:
+		return http.StatusNotImplemented
+	case Internal:
+		return http.StatusInternalServerError
+	case Unavailable:
+		return http.StatusServiceUnavailable
+	case DataLoss:
+		return http.StatusInternalServerError
+	}
+	return http.StatusInternalServerError
 }
