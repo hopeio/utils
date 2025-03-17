3863b5043590f95477433334e1a3cedffc0aadac
贾一饼 <lby.i@qq.com>
2025-03-17 14:08:55 +0800

    fix: mask

--- media/image/mask.go
+++ media/image/mask.go
@@ -26,7 +26,7 @@ func (m *BitMask) Set(x, y int, v bool) error {
 	if !(image.Point{X: x, Y: y}).In(m.Rect) {
 		return errors.New("out of range")
 	}
-	total := m.Rect.Dx()*y + x - m.Rect.Min.X
+	total := m.Rect.Dx()*(y-m.Rect.Min.Y) + x - m.Rect.Min.X
 	n := total / 8
 	bit := 8 - (total - n*8)
 	if bit == 0 {
@@ -45,7 +45,7 @@ func (m *BitMask) Get(x, y int) (bool, bool) {
 	if !(image.Point{X: x, Y: y}).In(m.Rect) {
 		return false, false
 	}
-	total := m.Rect.Dx()*y + x - m.Rect.Min.X
+	total := m.Rect.Dx()*(y-m.Rect.Min.Y) + x - m.Rect.Min.X
 	n := total / 8
 	bit := 8 - (total - n*8)
 	if bit == 0 {
@@ -57,29 +57,29 @@ func (m *BitMask) Get(x, y int) (bool, bool) {
 
 type Mask struct {
 	Data []uint8
-	rect image.Rectangle
+	Rect image.Rectangle
 }
 
 func NewMask(rect image.Rectangle) *Mask {
 	return &Mask{
-		rect: rect,
+		Rect: rect,
 		Data: make([]uint8, rect.Dx()*rect.Dy()),
 	}
 }
 
 func (m *Mask) Set(x, y int, v uint8) error {
-	if !(image.Point{X: x, Y: y}).In(m.rect) {
+	if !(image.Point{X: x, Y: y}).In(m.Rect) {
 		return errors.New("out of range")
 	}
-	n := m.rect.Dx()*y + x - m.rect.Min.X
+	n := m.Rect.Dx()*(y-m.Rect.Min.Y) + x - m.Rect.Min.X
 	m.Data[n] = v
 	return nil
 }
 
 func (m *Mask) Get(x, y int) (uint8, bool) {
-	if !(image.Point{X: x, Y: y}).In(m.rect) {
+	if !(image.Point{X: x, Y: y}).In(m.Rect) {
 		return 0, false
 	}
-	n := m.rect.Dx()*y + x - m.rect.Min.X
+	n := m.Rect.Dx()*(y-m.Rect.Min.Y) + x - m.Rect.Min.X
 	return m.Data[n], true
 }
--- media/image/mask_test.go
+++ media/image/mask_test.go
@@ -5,18 +5,13 @@ import (
 	"testing"
 )
 
-func TestSet_ValidPoint_ValueSetCorrectly(t *testing.T) {
+func TestBitMask(t *testing.T) {
 	rect := image.Rect(0, 0, 10, 10)
 	bitMask := NewBitMask(rect)
 	bitMask.Set(5, 5, true)
 	if bit, ok := bitMask.Get(5, 5); !ok || !bit {
 		t.Errorf("Expected value to be set to true, got false")
 	}
-}
-
-func TestSetAndGetValueAtBoundary_CorrectBehavior(t *testing.T) {
-	rect := image.Rect(0, 0, 10, 10)
-	bitMask := NewBitMask(rect)
 	bitMask.Set(0, 0, true)
 	if bit, ok := bitMask.Get(0, 0); !ok || !bit {
 		t.Errorf("Expected boundary value to be set to true, got false")
@@ -37,4 +32,21 @@ func TestSetAndGetValue_OutOfBounds_NoPanic(t *testing.T) {
 	}()
 	bitMask.Set(10, 10, true)
 	bitMask.Get(10, 10)
+}
+
+func TestMask(t *testing.T) {
+	rect := image.Rect(0, 0, 10, 10)
+	mask := NewMask(rect)
+	mask.Set(5, 5, 1)
+	if bit, ok := mask.Get(5, 5); !ok || bit != 1 {
+		t.Errorf("Expected value to be set to 1, got %d", bit)
+	}
+	mask.Set(0, 0, 2)
+	if bit, ok := mask.Get(0, 0); !ok || bit != 2 {
+		t.Errorf("Expected boundary value to be set to 2, got %d", bit)
+	}
+	mask.Set(9, 9, 3)
+	if bit, ok := mask.Get(9, 9); !ok || bit != 3 {
+		t.Errorf("Expected boundary value to be set to 3, got %d", bit)
+	}
 }
