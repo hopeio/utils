2aedd65e160e9c0c952637f260b071c3f0dc52c8
贾一饼 <lby.i@qq.com>
2025-03-24 09:38:27 +0800

    fiber: fiber header

---
+++ net/http/fiber/header.go
@@ -0,0 +1,68 @@
+package fiber
+
+import (
+	"github.com/hopeio/utils/strings"
+	"github.com/valyala/fasthttp"
+)
+
+type ResponseHeader struct {
+	*fasthttp.ResponseHeader
+}
+
+func (h ResponseHeader) Add(key, value string) {
+	h.ResponseHeader.Add(key, value)
+}
+
+func (h ResponseHeader) Set(key, value string) {
+	h.ResponseHeader.Set(key, value)
+}
+
+func (h ResponseHeader) Get(key string) string {
+	return strings.BytesToString(h.ResponseHeader.Peek(key))
+}
+
+func (h ResponseHeader) Values(key string) []string {
+	byteValues := h.ResponseHeader.PeekAll(key)
+	values := make([]string, len(byteValues))
+	for i := range byteValues {
+		values[i] = strings.BytesToString(byteValues[i])
+	}
+	return values
+}
+
+func (h ResponseHeader) Range(f func(key, value string)) {
+	h.ResponseHeader.VisitAll(func(key, value []byte) {
+		f(strings.BytesToString(key), strings.BytesToString(value))
+	})
+}
+
+type RequestHeader struct {
+	*fasthttp.RequestHeader
+}
+
+func (h RequestHeader) Add(key, value string) {
+	h.RequestHeader.Add(key, value)
+}
+
+func (h RequestHeader) Set(key, value string) {
+	h.RequestHeader.Set(key, value)
+}
+
+func (h RequestHeader) Get(key string) string {
+	return strings.BytesToString(h.RequestHeader.Peek(key))
+}
+
+func (h RequestHeader) Values(key string) []string {
+	byteValues := h.RequestHeader.PeekAll(key)
+	values := make([]string, len(byteValues))
+	for i := range byteValues {
+		values[i] = strings.BytesToString(byteValues[i])
+	}
+	return values
+}
+
+func (h RequestHeader) Range(f func(key, value string)) {
+	h.RequestHeader.VisitAll(func(key, value []byte) {
+		f(strings.BytesToString(key), strings.BytesToString(value))
+	})
+}
