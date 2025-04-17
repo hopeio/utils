3dc3c90398625afffca8e2c9e89c8cb6a42c5aee
贾一饼 <lby.i@qq.com>
2025-04-17 11:17:44 +0800

    fix: json datatype

--- dao/database/datatypes/json_test.go
+++ dao/database/datatypes/json_test.go
@@ -16,7 +16,7 @@ type Foo struct {
 }
 
 func TestJSONArrayT(t *testing.T) {
-	var jat ArrayJsonT[Foo]
+	var jat NullJson[[]Foo]
 	err := jat.Scan([]byte(`[{"A":1,"B":"1"},{"A":2,"B":"2"}]`))
 	if err != nil {
 		t.Error(err)
